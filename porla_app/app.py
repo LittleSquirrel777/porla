3d24633f3514803fff67b25d471c14c67f0e67b9
littlesquirrel777 <2436700441@qq.com>
2025-02-19 15:01:37 +0800

    添加了 返回从审计抽取的块

--- porla_app/app.py
+++ porla_app/app.py
@@ -14,84 +14,103 @@ import hashlib
 app = Flask(__name__)
 CORS(app)
 # databseName = "/..."
-serverPath = "/home/lishuai/porla/porla/porla/Server/Server"
-clientPath = "/home/lishuai/porla/porla/porla/Client/Client"
+serverPath = "/home/ls/porla/porla/porla/Server/Server"
+clientPath = "/home/ls/porla/porla/porla/Client/Client"
 serverOutputFile = "/data/ls/data_config_file/log/data_server_output"
 clientOutputFile = "/data/ls/data_config_file/log/data_client_output"
+audit_block_index_prefix = "/data/ls/data_config_file/log/audit_block_index_";
 
 databaseName_to_config_and_blockCount = {
-    "email_txt": {"config_path": "/data/ls/data_config_file/email_txt", "blockCount": 2097152, "highest_level_index": 21, "destroy_blocks": [], "pre_hash": [], "destroy_hash": []},
-    "img_m": {"config_path": "/data/ls/data_config_file/img_m", "blockCount": 4194304, "highest_level_index": 22, "destroy_blocks": [], "pre_hash": [], "destroy_hash": []},
-    "img_million": {"config_path": "/data/ls/data_config_file/img_m", "blockCount": 4194304, "highest_level_index": 22, "destroy_blocks": [], "pre_hash": [], "destroy_hash": []},
-    "text": {"config_path": "/data/ls/data_config_file/img_txt/text", "blockCount": 8192, "highest_level_index": 13, "destroy_blocks": [], "pre_hash": [], "destroy_hash": []},
+    "email_txt": {"config_path": "/data/ls/data_config_file/email_txt", "showBlockCount": 536870912, "destroy_blocks": [], "pre_hash": {}, "destroy_hash": {}, "blockCount": 2097152, "highest_level_index": 21},
+    "img_m": {"config_path": "/data/ls/data_config_file/img_m", "showBlockCount": 8388608, "destroy_blocks": [], "pre_hash": {}, "destroy_hash": {}, "blockCount": 4194304, "highest_level_index": 22},
+    "img_million": {"config_path": "/data/ls/data_config_file/img_txt/img_million", "showBlockCount": 8388608, "destroy_blocks": [], "pre_hash": {}, "destroy_hash": {}, "blockCount": 4194304, "highest_level_index": 22},
+    "text": {"config_path": "/data/ls/data_config_file/img_txt/text", "showBlockCount": 16384, "destroy_blocks": [], "pre_hash": {}, "destroy_hash": {}, "blockCount": 8192, "highest_level_index": 13},
 }
 
 # destroy_block_path = "/home/kt3/porla/porla/porla/Server/destroy_block"
 destroy_log_path = "/data/ls/data_config_file/log/destroy_log.txt"
-@app.route('/audit', methods=['POST', 'GET'])
-@cross_origin(origins=["http://10.201.153.203:8001"], methods=["GET", "POST"], allow_headers=["Content-Type"], withCredentials=True)
+@app.route('/audit', methods=['POST', 'GET', 'OPTIONS'])
+@cross_origin(origins=["http://10.201.153.99:9001", "http://127.0.0.1:9001", "http://localhost:9001", "http://10.201.97.165"], methods=["GET", "POST", "OPTIONS"], allow_headers=["Content-Type"], withCredentials=True)
 def audit():
-    if request.method == 'POST':
-        data = request.get_json(silent=True)
-        print(data)
-        result = audit_process(data)
-        with open(destroy_log_path, "+a", encoding="utf-8") as file:
-            file.writelines(f"{data['databaseName']} audit\n")
-        return jsonify(result)
-    else:
-        return " 'it's not a POST operation! "
+    # if request.method == 'POST':
+    data = request.get_json(silent=True)
+    print(data)
+    result = audit_process(data)
+    with open(destroy_log_path, "+a", encoding="utf-8") as file:
+        file.writelines(f"{data['databaseName']} audit\n")
+    return jsonify(result)
+    # else:
+    #     return " 'it's not a POST operation! "
 
 
-@app.route('/destroy_location', methods=['POST', 'GET'])
-@cross_origin(origins=["http://10.201.153.203:8001"], methods=["GET", "POST"], allow_headers=["Content-Type"], withCredentials=True)
+@app.route('/destroy_location', methods=['POST', 'GET', 'OPTIONS'])
+@cross_origin(origins=["http://10.201.153.99:9001", "http://127.0.0.1:9001", "http://localhost:9001"], methods=["GET", "POST", "OPTIONS"], allow_headers=["Content-Type"], withCredentials=True)
 def destroy_location():
-    if request.method == 'POST':
-        data = request.get_json(silent=True)
-        retData = {
-            "location": databaseName_to_config_and_blockCount[data["databaseName"]]["destroy_blocks"],
-            "pre_hash": databaseName_to_config_and_blockCount[data["databaseName"]]["pre_hash"],
-            "des_hash": databaseName_to_config_and_blockCount[data["databaseName"]]["destroy_hash"],
-        }
-        with open(destroy_log_path, "+a", encoding="utf-8") as file:
-            file.writelines(f"{data['databaseName']} location\n")
-        return jsonify(retData)
-    else:
-        return " 'it's not a POST operation! "
+    # if request.method == 'POST':
+    data = request.get_json(silent=True)
+    
+    audit_block_index = None
+    audit_block_index_path = audit_block_index_prefix + f"{data['databaseName']}"
+    with open(audit_block_index_path) as file:
+        audit_block_index = [int(line.strip()) for line in file]
+        audit_block_index.sort()
+    # blockCount = databaseName_to_config_and_blockCount[data["databaseName"]]["blockCount"]
+    destroy_blocks = databaseName_to_config_and_blockCount[data["databaseName"]]["destroy_blocks"]
+    # destroy_blocks.sort()
+    # destroy_blocks_hy = [i + blockCount for i in destroy_blocks_hx]
+    sameIndex = list(set(destroy_blocks) & set(audit_block_index))
+    pre_hash = databaseName_to_config_and_blockCount[data["databaseName"]]["pre_hash"]
+    des_hash =  databaseName_to_config_and_blockCount[data["databaseName"]]["destroy_hash"]
+    pre_hash = [pre_hash[str(i)] for i in sameIndex]
+    des_hash = [des_hash[str(i)] for i in sameIndex]
+    # print(len(sameIndex))
+    retData = {
+        "audit_location": list(set(audit_block_index)),
+        "des_location" : sameIndex,
+        "pre_hash": pre_hash,
+        "des_hash": des_hash,
+    }
+    
+    with open(destroy_log_path, "+a", encoding="utf-8") as file:
+        file.writelines(f"{data['databaseName']} location\n")
+    return jsonify(retData)
+    # else:
+    #     return " 'it's not a POST operation! "
 
-@app.route('/destroy', methods=['POST', 'GET'])
-@cross_origin(origins=["http://10.201.153.203:8001"], methods=["GET", "POST"], allow_headers=["Content-Type"], withCredentials=True)
+@app.route('/destroy', methods=['POST', 'GET', 'OPTIONS'])
+@cross_origin(origins=["http://10.201.153.99:9001", "http://127.0.0.1:9001", "http://localhost:9001"], methods=["GET", "POST", "OPTIONS"], allow_headers=["Content-Type"], withCredentials=True)
 def destroy():
-    if request.method == 'POST':
-        data = request.get_json(silent=True)
-        destroy_process(data)
-        retData = {
-            "location": databaseName_to_config_and_blockCount[data["databaseName"]]["destroy_blocks"],
-            "pre_hash": databaseName_to_config_and_blockCount[data["databaseName"]]["pre_hash"],
-            "des_hash": databaseName_to_config_and_blockCount[data["databaseName"]]["destroy_hash"],
-        }
-        with open(destroy_log_path, "+a", encoding="utf-8") as file:
-            file.writelines(f"{data['databaseName']} destroy\n")
-            json.dump(retData, file, ensure_ascii=False)
-            file.writelines("\n")
-        return jsonify(retData)
+    # if request.method == 'POST':
+    data = request.get_json(silent=True)
+    destroy_process(data)
+    retData = {
+        "location": databaseName_to_config_and_blockCount[data["databaseName"]]["destroy_blocks"],
+        "pre_hash": databaseName_to_config_and_blockCount[data["databaseName"]]["pre_hash"],
+        "des_hash": databaseName_to_config_and_blockCount[data["databaseName"]]["destroy_hash"],
+    }
+    with open(destroy_log_path, "+a", encoding="utf-8") as file:
+        file.writelines(f"{data['databaseName']} destroy\n")
+        json.dump(retData, file, ensure_ascii=False)
+        file.writelines("\n")
+    return jsonify(retData)
     
 
-@app.route('/recovery', methods=['POST', 'GET'])
-@cross_origin(origins=["http://10.201.153.203:8001"], methods=["GET", "POST"], allow_headers=["Content-Type"], withCredentials=True)
+@app.route('/recovery', methods=['POST', 'GET', 'OPTIONS'])
+@cross_origin(origins=["http://10.201.153.99:9001", "http://127.0.0.1:9001", "http://localhost:9001"], methods=["GET", "POST", "OPTIONS"], allow_headers=["Content-Type"], withCredentials=True)
 def recovery():
-    if request.method == 'POST':
-        data = request.get_json(silent=True)
-        retData = {}
-        if len(databaseName_to_config_and_blockCount[data["databaseName"]]["destroy_blocks"]) == 0:
-            retData["state"] = "failed"
-        else:
-            recovery_process(data)
-            retData["state"] = "success"
-        with open(destroy_log_path, "+a", encoding="utf-8") as file:
-            file.writelines(f"{data['databaseName']} recovery\n")
-            json.dump(retData, file, ensure_ascii=False)
-            file.writelines("\n")
-        return jsonify(retData)
+    # if request.method == 'POST':
+    data = request.get_json(silent=True)
+    retData = {}
+    if len(databaseName_to_config_and_blockCount[data["databaseName"]]["destroy_blocks"]) == 0:
+        retData["state"] = "failed"
+    else:
+        recovery_process(data)
+        retData["state"] = "success"
+    with open(destroy_log_path, "+a", encoding="utf-8") as file:
+        file.writelines(f"{data['databaseName']} recovery\n")
+        json.dump(retData, file, ensure_ascii=False)
+        file.writelines("\n")
+    return jsonify(retData)
 
 def audit_process(data):
     databaseName = data["databaseName"]
@@ -105,7 +124,14 @@ def audit_process(data):
         if returncode != 0:
             raise subprocess.CalledProcessError(returncode, clientProcess)
     except Exception as e:
-        return {"blockCount": databaseName_to_config_and_blockCount[data["databaseName"]]["blockCount"], "auditResult": "Failed", "totalAuditTime": "-",
+        audit_block_index_path = audit_block_index_prefix + f"{data['databaseName']}"
+        with open(audit_block_index_path) as file:
+            audit_block_index = [int(line.strip()) for line in file]
+            audit_block_index.sort()
+            # print(audit_block_index)
+        return {"blockCount": databaseName_to_config_and_blockCount[data["databaseName"]]["showBlockCount"], 
+            "audit_block_index" : audit_block_index,
+            "auditResult": "Failed", "totalAuditTime": "-",
             "readTime": "-", "computeTime": "-", "preparationTime": "-", "proveTime": "-"
         }
     finally:
@@ -130,7 +156,7 @@ def audit_process(data):
         line = file.readline()
         _, proveTime = line.strip().split(":")
         proveTime = float(proveTime) / 1000
-    data = {"blockCount": databaseName_to_config_and_blockCount[data["databaseName"]]["blockCount"], "auditResult": "Success", "totalAuditTime": str(totalAuditTime) + "ms", 
+    data = {"blockCount": databaseName_to_config_and_blockCount[data["databaseName"]]["showBlockCount"], "auditResult": "Success", "totalAuditTime": str(totalAuditTime) + "ms", 
         "readTime": str(readTime) + "ms", "computeTime": str(computeTime) + "ms", "preparationTime": str(preparationTime) + "ms", "proveTime": str(proveTime) + "ms"
     }
     
@@ -145,6 +171,7 @@ def destroy_process(data):
         with open(destroy_log_path, "+a", encoding="utf-8") as file:
             file.writelines("last database not recovery!!!\n")
         flag = True
+        return 
     config_path = databaseName_to_config_and_blockCount[data["databaseName"]]["config_path"]
     blockCount = databaseName_to_config_and_blockCount[data["databaseName"]]["blockCount"]
     level = databaseName_to_config_and_blockCount[data["databaseName"]]["highest_level_index"]
@@ -168,25 +195,31 @@ def destroy_process(data):
     #             futures = []
     #     for future in concurrent.futures.as_completed(futures):
     #         future.result()
-    for i in range(int(blockCount / 100)):
+    for i in range(int(blockCount * 0.003)):
         destroyNum = random.randint(0, blockCount - 1)
-        if not flag:
-            while destroyNum in destroyBlocks:
-                destroyNum = random.randint(0, blockCount - 1)
+        while destroyNum in destroyBlocks:
+            destroyNum = random.randint(0, blockCount - 1)
         if destroyNum not in destroyBlocks:
-            destroy_subprocess(destroyNum, config_path, level, destroyBlocks, pre_hash, destroy_hash)
+            destroy_subprocess(data, destroyNum, config_path, level, destroyBlocks, pre_hash, destroy_hash)
     print(len(destroyBlocks))
     print(len(pre_hash))
     print(len(destroy_hash))
 
-def destroy_subprocess(destroyNum, config_path, level, destroyBlocks, pre_hash, destroy_hash):
+def destroy_subprocess(data, destroyNum, config_path, level, destroyBlocks, pre_hash, destroy_hash):
+    blockCount = databaseName_to_config_and_blockCount[data["databaseName"]]["blockCount"]
     destroyBlocks.append(destroyNum)
+    destroyBlocks.append(destroyNum + blockCount)
     hash_value = calculate_file_hash(f"{config_path}/H_X/{level}_{destroyNum}")
-    pre_hash.append(hash_value)
+    # print(type(pre_hash))
+    pre_hash[str(destroyNum)] = hash_value
+    hash_value = calculate_file_hash(f"{config_path}/H_Y/{level}_{destroyNum}")
+    pre_hash[str(destroyNum + blockCount)] = hash_value
     write_number_to_first_line(f"{config_path}/H_X/{level}_{destroyNum}")
     write_number_to_first_line(f"{config_path}/H_Y/{level}_{destroyNum}")
-    destroy_hash_value = calculate_file_hash(f"{config_path}/H_X/{level}_{destroyNum}")
-    destroy_hash.append(destroy_hash_value)
+    hash_value = calculate_file_hash(f"{config_path}/H_X/{level}_{destroyNum}")
+    destroy_hash[str(destroyNum)] = hash_value
+    hash_value = calculate_file_hash(f"{config_path}/H_Y/{level}_{destroyNum}")
+    destroy_hash[str(destroyNum + blockCount)] = hash_value
 
 def recovery_process(data):
     config_path = databaseName_to_config_and_blockCount[data["databaseName"]]["config_path"]
@@ -203,11 +236,11 @@ def recovery_process(data):
     #             futures = []
     #     for future in concurrent.futures.as_completed(futures):
     #         future.result()
-    for destroyNum in destroyBlocks:
-        recovery_subprocess(destroyNum, config_path, level)
+    for destroyNum in range(0, len(destroyBlocks), 2):
+        recovery_subprocess(destroyBlocks[destroyNum], config_path, level)
     databaseName_to_config_and_blockCount[data["databaseName"]]["destroy_blocks"] = []
-    databaseName_to_config_and_blockCount[data["databaseName"]]["pre_hash"] = []
-    databaseName_to_config_and_blockCount[data["databaseName"]]["destroy_hash"] = []
+    databaseName_to_config_and_blockCount[data["databaseName"]]["pre_hash"] = {}
+    databaseName_to_config_and_blockCount[data["databaseName"]]["destroy_hash"] = {}
 
 def recovery_subprocess(destroyNum, config_path, level):
     remove_first_line(f"{config_path}/H_X/{level}_{destroyNum}")
